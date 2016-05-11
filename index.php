fcf9ca68e072b2244e79b4e7f52b79c415b1807e
Sebastian Velasquez <sevelasquez@alumnos.uai.cl>
2016-05-11 14:19:44 -0300

    asdasd

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php require_once (dirname ( __FILE__ ) . '/../../config.php');?>
+<?php require_once (dirname ( __FILE__ ) . '/../../../config.php');?>
 <!DOCTYPE unspecified PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <link rel="stylesheet" href="estilo.css">
