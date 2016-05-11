a1e7032a92eedaf1ab0abf3ab449f90d5ad37189
Sebastian Velasquez <sevelasquez@alumnos.uai.cl>
2016-05-11 17:05:44 -0300

    index

--- index.php
+++ index.php
@@ -23,19 +23,8 @@
 // Minimum for Moodle to work, the basic libraries
 require_once (dirname ( __FILE__ ) . '/../../../config.php');
 // Parameter passed from the url.
-$name = required_param('name', PARAM_TEXT);
-// Moodle pages require a context, that can be system, course or module (activity or resource)
 $context = context_system::instance();
 $PAGE->set_context($context);
 // Check that user is logued in the course.
 require_login();
 // Page navigation and URL settings.
-$PAGE->set_url(new moodle_url('/local/tics331', array('filter'=>$name)));
-$PAGE->set_pagelayout('incourse');
-$PAGE->set_title('Hello world');
-// Show the page header
-echo $OUTPUT->header();
-// Here goes the content
-echo 'Hello world';
-// Show the page footer
-echo $OUTPUT->footer();
