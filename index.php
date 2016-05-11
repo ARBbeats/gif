f175a9b16dd5a84764c45e914cae786a9fde4a5e
Sebastian Velasquez <sevelasquez@alumnos.uai.cl>
2016-05-11 13:10:52 -0300

    prueba de página 1

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
-echo "googogogogo";
+$PAGE->set_url($urlemarking);
+$PAGE->set_context($context);
+$PAGE->set_course($course);
+$PAGE->set_pagelayout('incourse');
+$PAGE->set_cm($cm);
+$PAGE->set_title(get_string('emarking', 'mod_emarking'));
+// Require jquery for modal.
+$PAGE->requires->jquery();
+$PAGE->requires->jquery_plugin('ui');
+$PAGE->requires->jquery_plugin('ui-css');
