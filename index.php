4c39cc968b3841b75cd9ca26560092361e0d9c57
Sebastian Velasquez <sevelasquez@alumnos.uai.cl>
2016-05-11 16:23:16 -0300

    ajustes index

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <?php require_once (dirname ( __FILE__ ) . '/../../../config.php');?>
 <!DOCTYPE unspecified PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
@@ -10,39 +9,26 @@ GiF
 <div id="linea">
 </div>
 <div id="header">
-	<div id="logos">
+	<div id="logo"> // logo de la esquina derecha, que redirige a home
 			<a class="logo" href="<?php echo $CFG->wwwroot . '/local/gif/gif/index.php'; ?> ">
 				<img src="images/gil.gif">
 			</a>
-		<div class="logo2">
-		</div>
+		
 	</div>
-</div>
-
-<div id="linea">
-</div>
-
-<div id="banner">
-</div>
-
-<div id="linea2">
 </div>
 
 <div id="main">
 	<div id="container1" class="clearfix">
-		<a class="dest cat1" href="<?php echo $CFG->wwwroot . '/local/tutoriales/profesores.php'; ?>">
+		<a class="dest cat1" href="<?php echo $CFG->wwwroot . '/local/gif/gif/profesores.php'; ?>">
  			<img width="256" height="256" src="images/profesoresg.png">
 		</a>
-		<a class="dest cat2" href="<?php echo $CFG->wwwroot . '/local/tutoriales/alumnos.php'; ?>">
+		<a class="dest cat2" href="<?php echo $CFG->wwwroot . '/local/gif/gif/alumnos.php'; ?>">
 			<img width="256" height="256" src="images/alumnosg.png">
 		</a>
 	
 	</div>
 	<div id="separador">
 	</div>
-
-<div id="linea2">
-</div>
 
 <div id="foot">
 	<div class="footer">&copy; <?php echo date('Y');?> Universidad Adolfo Ib&aacute;&ntilde;ez</div>
