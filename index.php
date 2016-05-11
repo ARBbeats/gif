dd26ec773c07ba9acbdb49a820116a04c5ccd461
Sebastian Velasquez <sevelasquez@alumnos.uai.cl>
2016-05-11 16:59:10 -0300

    hello world test

--- index.php
+++ index.php
@@ -1,36 +1,41 @@
-<?php require_once (dirname ( __FILE__ ) . '/../../../config.php');?>
-<!DOCTYPE unspecified PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<!---- estilo de pagina ---->
-<link rel="stylesheet" href="estilo.css">
-<title>
-GiF
-</title>
-<body>
-<div id="linea">
-</div>
-<!---- logo de inicio de la esquina derecha ---->
-<div id="header">
-	<div id="logo"> 
-			<a class="logo" href="<?php echo $CFG->wwwroot . '/local/gif/gif/index.php'; ?> ">
-				<img src="images/gil.gif">
-			</a>
-		
-	</div>
-</div>
-<!---- Login para alumno y profe ---->
-<div id="main">
-	<div id="container1" class="clearfix">
-		
-		<a class="dest cat2" href="<?php echo $CFG->wwwroot . '/local/gif/gif/ingreso.php'; ?>">
-			<img width="256" height="256" src="images/alumnosg.png">
-		</a>
-	
-	</div>
-	<div id="separador">
-	</div>
-<!---- logo de adolfo ibañez inferior ---->
-<div id="foot">
-	<div class="footer">&copy; <?php echo date('Y');?> Universidad Adolfo Ib&aacute;&ntilde;ez</div>
-
-</body>
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ *
+ * @package local
+ * @subpackage tics331
+ * @copyright 2012-onwards Jorge Villalon <jorge.villalon@uai.cl>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+// Minimum for Moodle to work, the basic libraries
+require_once(dirname(dirname(dirname(__FILE__))) . '/config.php');
+// Parameter passed from the url.
+$name = required_param('name', PARAM_TEXT);
+// Moodle pages require a context, that can be system, course or module (activity or resource)
+$context = context_system::instance();
+$PAGE->set_context($context);
+// Check that user is logued in the course.
+require_login();
+// Page navigation and URL settings.
+$PAGE->set_url(new moodle_url('/local/tics331', array('filter'=>$name)));
+$PAGE->set_pagelayout('incourse');
+$PAGE->set_title('Hello world');
+// Show the page header
+echo $OUTPUT->header();
+// Here goes the content
+echo 'Hello world';
+// Show the page footer
+echo $OUTPUT->footer();
