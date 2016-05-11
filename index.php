ef76103fcdc49ff6b1595a117d6428b970b5d9d9
Sebastian Velasquez <sevelasquez@alumnos.uai.cl>
2016-05-11 16:50:44 -0300

    index

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
  * @copyright  1999 onwards Martin Dougiamas (http://dougiamas.com)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-if (!file_exists('/../config.php')) {
+if (!file_exists('./././config.php')) {
     header('Location: install.php');
     die;
 }
