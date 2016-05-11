e740147c1a73ed448e8ff4165896c008df135ca8
Sebastian Velasquez <sevelasquez@alumnos.uai.cl>
2016-05-11 15:17:14 -0300

    elimine

--- versiones.php
+++
@@ -1,6 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-$plugin->version = 2015012300;
-$plugin->requires = 2010021900;
-$plugin->component = 'local_tutoriales';
-$plugin->pluginname = "local_tutoriales";
