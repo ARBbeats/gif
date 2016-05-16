49a1174062179b07c1e8c0e3351eb6c84612a729
Sebastian Velasquez <sevelasquez@alumnos.uai.cl>
2016-05-16 11:55:46 -0300

    index

--- index.php
+++ index.php
@@ -8,7 +8,10 @@ $context = context_system::instance();
 $PAGE->set_context($context);
 // Check that user is logued in the course.
 require_login();
-
+// Page navigation and URL settings.
+$PAGE->set_url(new moodle_url('local/gif/gif', array('filter'=>$name)));
+$PAGE->set_pagelayout('incourse');
+$PAGE->set_title('Hello world');
 // Show the page header
 echo $OUTPUT->header();
 // Here goes the content
