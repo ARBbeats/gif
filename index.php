42ef14d873585a9787d89a71e55e1b3aaeb26c13
Sebastian Velasquez <sevelasquez@alumnos.uai.cl>
2016-05-17 17:52:06 -0300

    si no esta logueado no se entra

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ *
+ * @package local
+ * @subpackage notebookstore
+ * @copyright 2016 Sebastian Velasquez (sevelasquez@alumnos.uai.cl)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 require_once (dirname ( __FILE__ ) . '/../../../config.php');
 // Parameter passed from the url.
 
@@ -8,15 +16,20 @@ $context = context_system::instance();
 $PAGE->set_context($context);
 // Check that user is logued in the course.
 require_login();
+if (isguestuser()) {
+	die();
+}
 // Page navigation and URL settings.
 $PAGE->set_url(new moodle_url('/local/gif/gif', array('filter'=>$name)));
 $PAGE->set_pagelayout('incourse');
-$PAGE->set_title('Hello world');
+$PAGE->set_title('GIF');
 // Show the page header
 echo $OUTPUT->header();
 // Here goes the content
-echo 'Hello world';
+
 // Show the page footer
 echo $OUTPUT->footer();
+
+
 
 ?>
