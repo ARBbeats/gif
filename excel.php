bdfe0cd09875a9e065822abffc6997ca68fe00c8
Sebastian Velasquez <sevelasquez@alumnos.uai.cl>
2016-05-25 01:40:46 -0300

    excel export

--- excel.php
+++ excel.php
@@ -1,14 +1,32 @@
 <?php
-$conexion = mysql_connect ("localhost", "root", "6500233");
-mysql_select_db ("moodle", $conexion);
-$sql = "select s.id as id,count(g.userid) as sum,(SELECT count(u.userid) FROM mdl_quiz_sections as s join mdl_user_enrolments as u on (s.id=u.enrolid))-count(u.enrolid) as fa
-from mdl_quiz_grades as g 
-join mdl_quiz_sections as s on (s.quizid=g.quiz)
-join mdl_user_enrolments as u on (u.userid=g.userid)
-group by s.id desc";
+/**
+ *
+ * @package local
+ * @subpackage excel export report
+ * @copyright 2016 Sebastian Velasquez (sevelasquez@alumnos.uai.cl)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
-$resultado = mysql_query ($sql, $conexion) or die (mysql_error ());
-$registros = mysql_num_rows ($resultado);
+require_once (dirname ( __FILE__ ) . '/../../../config.php');
+global $DB, $USER, $PAGE, $OUTPUT;
+// Parameter passed from the url.
+
+// Moodle pages require a context, that can be system, course or module (activity or resource)
+$context = context_system::instance();
+$PAGE->set_context($context);
+
+// Check that user is logued in the course.
+require_login();
+if (isguestuser()) {
+	die();
+}
+// Page navigation and URL settings.
+$PAGE->set_url(new moodle_url('/local/gif/gif', array('filter'=>$name)));
+$PAGE->set_pagelayout('incourse');
+$PAGE->set_title('GIF');
+// Show the page header
+echo $OUTPUT->header();
+
 
 $grupo = "select s.id as id,count(g.userid) as sum,(SELECT count(u.userid) FROM mdl_quiz_sections as s join mdl_user_enrolments as u on (s.id=u.enrolid))-count(u.enrolid) as fa
 from mdl_quiz_grades as g
@@ -53,5 +71,9 @@ header('Cache-Control: max-age=0');
 $objWriter=PHPExcel_IOFactory::createWriter($objPHPExcel,'Excel2007');
 $objWriter->save('php://output');
 exit;
-mysql_close ();
+
+
+// Show the page footer
+echo $OUTPUT->footer();
+
 ?>
