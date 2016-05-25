0c6c2c325aac53da5fdb0b88dd32747876e0818f
Sebastian Velasquez <sevelasquez@alumnos.uai.cl>
2016-05-25 00:59:29 -0300

    excel export

--- index.php
+++ index.php
@@ -63,6 +63,41 @@ foreach ($d as $data){
 }
 echo html_writer::table ( $tabla );
 
+require_once '/../../../PHPExcel_1.8.0/Classes/PHPExcel.php';
+
+if ($d > 0) {
+	require_once 'Classes/PHPExcel.php';
+	$objPHPExcel = new PHPExcel();
+	 
+	//Informacion del excel
+	$objPHPExcel->
+	getProperties()
+	->setCreator("ingenieroweb.com.co")
+	->setLastModifiedBy("ingenieroweb.com.co")
+	->setTitle("Exportar excel desde mysql")
+	->setSubject("Ejemplo 1")
+	->setDescription("Documento generado con PHPExcel")
+	->setKeywords("ingenieroweb.com.co  con  phpexcel")
+	->setCategory("ciudades");
+
+	$i = 1;
+	while ($registro = mysql_fetch_object ($d)) {
+		 
+		$objPHPExcel->setActiveSheetIndex(0)
+		->setCellValue('A'.$i, $registro->name);
+
+		$i++;
+
+	}
+}
+header('Content-Type: application/vnd.ms-excel');
+header('Content-Disposition: attachment;filename="ejemplo1.xlsx"');
+header('Cache-Control: max-age=0');
+
+$objWriter=PHPExcel_IOFactory::createWriter($objPHPExcel,'Excel2007');
+$objWriter->save('php://output');
+exit;
+
 // Show the page footer
 echo $OUTPUT->footer();
 
