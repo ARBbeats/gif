1c554ff45bfbd4f41d6b033529c29f4f355b5f0d
Sebastian Velasquez <sevelasquez@alumnos.uai.cl>
2016-05-25 01:42:50 -0300

    excel export

--- excel.php
+++ excel.php
@@ -7,24 +7,13 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-require_once (dirname ( __FILE__ ) . '/../../../config.php');
+
 global $DB, $USER, $PAGE, $OUTPUT;
 // Parameter passed from the url.
-
-// Moodle pages require a context, that can be system, course or module (activity or resource)
-$context = context_system::instance();
-$PAGE->set_context($context);
-
-// Check that user is logued in the course.
 require_login();
 if (isguestuser()) {
 	die();
 }
-// Page navigation and URL settings.
-$PAGE->set_url(new moodle_url('/local/gif/gif', array('filter'=>$name)));
-$PAGE->set_pagelayout('incourse');
-$PAGE->set_title('GIF');
-// Show the page header
 echo $OUTPUT->header();
 
 
@@ -71,9 +60,5 @@ header('Cache-Control: max-age=0');
 $objWriter=PHPExcel_IOFactory::createWriter($objPHPExcel,'Excel2007');
 $objWriter->save('php://output');
 exit;
-
-
-// Show the page footer
-echo $OUTPUT->footer();
 
 ?>
