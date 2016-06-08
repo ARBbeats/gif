8ded40f8652d1684740db1fcd6df5e261d3388f7
Sebastian Velasquez <sevelasquez@alumnos.uai.cl>
2016-06-07 23:23:38 -0300

    comentarios

--- excel.php
+++ excel.php
@@ -1,15 +1,23 @@
 <?php
+
+// conexion con base de datos
  $conexion = mysql_connect ("localhost", "root", "6500233");
  mysql_select_db ("moodle", $conexion);    
+ 
+ //consulta para pedir los datos necesarios y agregarlos al excel
  $sql = "select s.id as id,count(g.userid) as sum,(SELECT count(u.userid) FROM mdl_quiz_sections as s join mdl_user_enrolments as u on (s.id=u.enrolid))-count(u.enrolid) as fa
 from mdl_quiz_grades as g
 join mdl_quiz_sections as s on (s.quizid=g.quiz)
 join mdl_user_enrolments as u on (u.userid=g.userid)
 group by s.id desc";
+ 
+ //ejecutar consulta sql
  $resultado = mysql_query ($sql, $conexion) or die (mysql_error ());
  $registros = mysql_num_rows ($resultado);
  
+ //pregunta si existe algo en la variable registros
  if ($registros > 0) {
+ 	//llamo a la libreria excel
    require_once '/../../../PHPExcel_1.8.0/Classes/PHPExcel.php';
    $objPHPExcel = new PHPExcel();
    
@@ -25,6 +33,9 @@ group by s.id desc";
         ->setCategory("Datos");    
 
    $i = 1;    
+   
+   //insertamos los datos de la variable registros al excel, en las columnas que sean
+   //las adecuadas
    while ($registros = mysql_fetch_object ($resultado)) {
        
       $objPHPExcel->setActiveSheetIndex(0)
@@ -35,6 +46,8 @@ group by s.id desc";
       
    }
 }
+
+//informacion del documento que será generado
 header('Content-Type: application/vnd.ms-excel');
 header('Content-Disposition: attachment;filename="Analisis.xls"');
 header('Cache-Control: max-age=0');
